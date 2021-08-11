2d6577450a751e1de5e2c32a79773f5fc8da4e0c
LZZ383 <59161887+LZZ383@users.noreply.github.com>
2021-08-11 14:09:05 +0800

    Cmake构建多文件编译

---
+++ build/CMakeFiles/my_cmake_swap.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/NUAA/Desktop/G++_Cmake_git_practice/main.cpp" "C:/Users/NUAA/Desktop/G++_Cmake_git_practice/build/CMakeFiles/my_cmake_swap.dir/main.cpp.obj"
+  "C:/Users/NUAA/Desktop/G++_Cmake_git_practice/swap.cpp" "C:/Users/NUAA/Desktop/G++_Cmake_git_practice/build/CMakeFiles/my_cmake_swap.dir/swap.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
